59caa848bbe8ac441566be6efbde222f04d809fe
Viktor <esteve.viktor@gmail.com>
2019-05-22 20:22:31 -0400

    add docker

--- bestdeal/api/tag/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import re_path
-from . views import TagListView, TagDetailView
-
-
-urlpatterns = [
-    # sans deal
-    re_path(r'^tags/$', TagListView.as_view(), name='tag-list-nofk'),
-    re_path(r'^tags/(?P<pk>[0-9a-f-]+)/$', TagDetailView.as_view(), name='tag-detail'),
-]
--- bestdeal/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BestdealConfig(AppConfig):
-    name = 'bestdeal'
