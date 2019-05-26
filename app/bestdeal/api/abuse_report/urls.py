df125cd27e5de09246eac59d89d95d180a0d3b24
Viktor <esteve.viktor@gmail.com>
2019-05-25 21:28:27 -0400

    build docker backend, frontend, & nginx

--- app/bestdeal/api/abuse_report/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-from . views import AbuseListView, AbuseDetailView
-
-
-urlpatterns = [
-    re_path(r'^abuse-report/$', AbuseListView.as_view(), name='abuse-list'),
-    re_path(r'^abuse-report/(?P<pk>[0-9a-f-]+)/$', AbuseDetailView.as_view(), name='abuse-detail'),
-]
--- app/bestdeal/models.py
+++
@@ -1,6 +0,0 @@
-from . api.deal.models import Deal
-from . api.marque.models import Marque
-from . api.tag.models import Tag
-from . api.commentaire.models import Comment
-from . api.abuse_report.models import AbuseReport
-
